9a54bff13d9a3e3f8f3e5b75b3c79fbcfcf53c72
ydagosto <ydagosto@gmail.com>
2024-09-03 03:31:11 -0700

    fixing formatting

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,20 +4,20 @@ import { useEffect } from 'react';
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center p-4">
-      <h1 className="text-white text-3xl md:text-4xl font-bold mb-4 text-center">
+      <h1 className="text-gray-700 text-3xl md:text-4xl font-bold mb-4 text-center">
         Welcome to tenthousandhours
       </h1>
-      <p className="text-white text-base md:text-lg mb-6 text-center">
+      <p className="text-gray-700 text-base md:text-lg mb-6 text-center">
         Master Your Skills, One Hour at a Time.
       </p>
       <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4 items-center">
-        <a href="/register" className="text-white underline">
+        <a href="/register" className="text-gray-700 underline">
           Register
         </a>
-        <a href="/login" className="text-white underline">
+        <a href="/login" className="text-gray-700 underline">
           Login
         </a>
-        <a href="/dashboard" className="text-white underline">
+        <a href="/dashboard" className="text-gray-700 underline">
           Dashboard
         </a>
       </div>
