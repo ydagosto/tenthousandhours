1e8b3cd932e1b04a5c0552170ad50262eb310798
ydagosto <ydagosto@gmail.com>
2024-09-02 17:40:15 -0700

    fixing up front-end welcome page

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors : {
+        lightblue: {
+          500: '#ADD8E6', // Custom light blue color
+        },
+      }
     },
   },
   plugins: [],
