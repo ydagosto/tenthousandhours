4f4bad81ab8c162b56185cf6719755e263fa8c3e
ydagosto <ydagosto@gmail.com>
2024-09-03 02:59:35 -0700

    changing background color

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body >{children}</body>
+      <body className="bg-lightblue-500">{children}</body>
     </html>
   );
 }
