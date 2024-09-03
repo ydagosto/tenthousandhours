c1888d964dfd25adf61fed0cad760c8f5333d024
ydagosto <ydagosto@gmail.com>
2024-09-02 18:13:19 -0700

    mobile-friendly home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,13 +3,23 @@ import { useEffect } from 'react';
 
 export default function Home() {
   return (
-    <div className="bg-lightblue-500 min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-white text-4xl font-bold mb-4">Welcome to Tenthousandhours</h1>
-      <p className="text-white text-lg mb-6">This is the homepage of your application.</p>
-      <div className="flex space-x-4">
-        <a href="/register" className="text-white underline">Register</a>
-        <a href="/login" className="text-white underline">Login</a>
-        <a href="/dashboard" className="text-white underline">Dashboard</a>
+    <div className="bg-lightblue-500 min-h-screen flex flex-col items-center justify-center p-4">
+      <h1 className="text-white text-3xl md:text-4xl font-bold mb-4 text-center">
+        Welcome to tenthousandhours
+      </h1>
+      <p className="text-white text-base md:text-lg mb-6 text-center">
+        Master Your Skills, One Hour at a Time.
+      </p>
+      <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4 items-center">
+        <a href="/register" className="text-white underline">
+          Register
+        </a>
+        <a href="/login" className="text-white underline">
+          Login
+        </a>
+        <a href="/dashboard" className="text-white underline">
+          Dashboard
+        </a>
       </div>
     </div>
   );
