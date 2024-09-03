e7e283dcd0b217a70fab155b7b50030593e3e5c1
ydagosto <ydagosto@gmail.com>
2024-09-02 17:40:15 -0700

    fixing up front-end welcome page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,11 +3,14 @@ import { useEffect } from 'react';
 
 export default function Home() {
   return (
-      <div>
-        <h1>Welcome to Tenthousandhours</h1>
-        <p>This is the homepage of your application.</p>
-        <a href="/register">Register</a>
-        <a href="/login">Login</a>
-        <a href="/dashboard">Register</a>
-      </div>);
+    <div className="bg-lightblue-500 min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-white text-4xl font-bold mb-4">Welcome to Tenthousandhours</h1>
+      <p className="text-white text-lg mb-6">This is the homepage of your application.</p>
+      <div className="flex space-x-4">
+        <a href="/register" className="text-white underline">Register</a>
+        <a href="/login" className="text-white underline">Login</a>
+        <a href="/dashboard" className="text-white underline">Dashboard</a>
+      </div>
+    </div>
+  );
 }
