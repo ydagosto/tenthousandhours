12ff3fa88102e06d6c3c205d614f562bc7c66cda
ydagosto <ydagosto@gmail.com>
2024-09-05 12:31:14 +0200

    update button

--- frontend/src/components/UpdateUserInfo.tsx
+++ frontend/src/components/UpdateUserInfo.tsx
@@ -102,7 +102,6 @@ export default function UpdateUserInfo() {
                     variant="contained"
                     color="primary"
                     type="submit"
-                    fullWidth
                     disabled={loading}
                 >
                     {loading ? 'Saving...' : 'Save Profile'}
